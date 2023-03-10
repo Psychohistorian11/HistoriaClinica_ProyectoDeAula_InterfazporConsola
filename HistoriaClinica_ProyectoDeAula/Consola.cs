4bc4fc58e83ab8604e1b9e9ee9594bb2e7ac7623
Cristian <cristianfrancoraigosa@gmail.com>
2023-03-10 13:09:25 -0500

    Primera Estructura, clases y metodos del proyecto, posible a recibir cambios

---
+++ HistoriaClinica_ProyectoDeAula/Consola.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoriaClinica_ProyectoDeAula
+{
+    internal class Consola
+    {
+        Clinica clinica = new Clinica();
+
+        public void ingresarPaciente()
+        {
+
+        }
+        public void CambiarEPS()
+        {
+
+        }
+        public void MostrarEstadisticas()
+        {
+
+        }
+    }
+}
---
+++ HistoriaClinica_ProyectoDeAula/Mundo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace HistoriaClinica_ProyectoDeAula
+{
+    public class Persona
+    {
+        int identificacion;
+        string nombre;
+        string apellidos;
+        DateTime fechaNacimiento;
+        string tipoRegimen;
+        int semanasCotizadas;
+        DateTime fechaIngreso;
+        DateTime fechaIngresoEPS;
+        string EPS;
+        string historiaClinica;
+        int cantidadEnfermedades;
+        string enfermedadRelevante;
+        string tipoAfiliacion;
+        int costosTratamientos;
+
+        public Persona(int identificacion, 
+            string nombre, 
+            string apellidos,
+            DateTime fechaNacimiento, 
+            string tipoRegimen, 
+            int semanasCotizadas, 
+            DateTime fechaIngreso, 
+            DateTime fechaIngresoEPS, 
+            string ePS, 
+            string historiaClinica, 
+            int cantidadEnfermedades, 
+            string enfermedadRelevante, 
+            string tipoAfiliacion, 
+            int costosTratamientos)
+        {
+            this.Identificacion = identificacion;
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.FechaNacimiento = fechaNacimiento;
+            this.TipoRegimen = tipoRegimen;
+            this.SemanasCotizadas = semanasCotizadas;
+            this.FechaIngreso = fechaIngreso;
+            this.FechaIngresoEPS = fechaIngresoEPS;
+            EPS1= ePS;
+            this.HistoriaClinica = historiaClinica;
+            this.CantidadEnfermedades = cantidadEnfermedades;
+            this.EnfermedadRelevante = enfermedadRelevante;
+            this.TipoAfiliacion = tipoAfiliacion;
+            this.CostosTratamientos = costosTratamientos;
+        }
+
+        public int Identificacion { get => identificacion; set => identificacion = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public string TipoRegimen { get => tipoRegimen; set => tipoRegimen = value; }
+        public int SemanasCotizadas { get => semanasCotizadas; set => semanasCotizadas = value; }
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+        public DateTime FechaIngresoEPS { get => fechaIngresoEPS; set => fechaIngresoEPS = value; }
+        public string EPS1 { get => EPS; set => EPS = value; }
+        public string HistoriaClinica { get => historiaClinica; set => historiaClinica = value; }
+        public int CantidadEnfermedades { get => cantidadEnfermedades; set => cantidadEnfermedades = value; }
+        public string EnfermedadRelevante { get => enfermedadRelevante; set => enfermedadRelevante = value; }
+        public string TipoAfiliacion { get => tipoAfiliacion; set => tipoAfiliacion = value; }
+        public int CostosTratamientos { get => costosTratamientos; set => costosTratamientos = value; }
+
+
+
+    }
+
+    public class Clinica
+    {
+        public int calcularPorcentajePacienteSinEnfermedad()
+        {
+            return int;
+        }
+
+        public double calcularTotalCostosEPS()
+        {
+            return double;
+        }
+
+        public void cambiarEPS()
+        {
+
+        }
+        public double calcularPorcentajeCostosEPS()
+        {
+            return double;
+        }
+        public int calcularTotalPacientes()
+        {
+            return int
+                
+        }
+        public double calcularPorcentajesPorEdad()
+        {
+            return double;
+        }
+        public void encontrarMayorCosto()
+        {
+
+        }
+        public double calcularPacientesPorRegimen()
+        {
+            return double;
+        }
+        public double calcularPorcentajePacientesPorTipoAfiliacion()
+        {
+            return double;
+        }
+    }
+}
